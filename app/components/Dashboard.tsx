a5ff11924bf0ab7c7843d52840d4c99016cd3663
martinbolsnes <martinbolsones@gmail.com>
2025-02-15 20:33:52 +0100

    create dashboard for view and manage list

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -2,12 +2,22 @@
 
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
+import { useAuth } from '@clerk/nextjs';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Plus } from 'lucide-react';
 import { Card, CardHeader, CardTitle } from '@/components/ui/card';
 import { LoadingSpinner } from './LoadingSpinner';
-import { useAuth, useUser } from '@clerk/nextjs';
+import { toast } from '@/hooks/use-toast';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { Badge } from '@/components/ui/badge';
 
 interface List {
   _id: string;
@@ -16,31 +26,14 @@ interface List {
 
 export default function Dashboard() {
   const { isLoaded, isSignedIn } = useAuth();
-  const { user } = useUser();
 
   const [lists, setLists] = useState<List[]>([]);
   const [newListName, setNewListName] = useState('');
   const [loadingLists, setLoadingLists] = useState(false);
   const [creatingList, setCreatingList] = useState(false);
+  const [selectedList, setSelectedList] = useState<List | null>(null);
+  const [isDialogOpen, setDialogOpen] = useState(false);
 
-  // Ensure user exists in MongoDB
-  useEffect(() => {
-    if (user) {
-      fetch('/api/users', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          clerkId: user.id,
-          email: user.primaryEmailAddress?.emailAddress,
-          name: user.firstName,
-        }),
-      }).catch((err) => console.error('Error syncing user:', err));
-    }
-  }, [user]);
-
-  // Fetch lists
   useEffect(() => {
     fetchLists();
   }, []);
@@ -54,7 +47,7 @@ export default function Dashboard() {
       const data = await response.json();
       setLists(data);
     } catch (error) {
-      console.error('Error fetching lists:', error);
+      toast({ title: 'Feil', description: 'Klarte ikke hente listene' });
     } finally {
       setLoadingLists(false);
     }
@@ -76,60 +69,128 @@ export default function Dashboard() {
       setNewListName('');
       fetchLists();
     } catch (error) {
-      console.error('Error creating list:', error);
+      toast({ title: 'Feil', description: 'Klarte ikke opprette listen' });
     } finally {
       setCreatingList(false);
     }
   };
 
+  const handleDeleteClick = (list: List) => {
+    setSelectedList(list);
+    setDialogOpen(true);
+  };
+
+  const confirmDelete = async () => {
+    if (!selectedList) return;
+
+    try {
+      const response = await fetch(`/api/lists/${selectedList._id}`, {
+        method: 'DELETE',
+      });
+
+      if (!response.ok) throw new Error('Failed to delete list');
+
+      setLists((prevLists) =>
+        prevLists.filter((list) => list._id !== selectedList._id)
+      );
+      toast({ title: 'Slettet', description: 'Listen ble slettet' });
+    } catch (error) {
+      toast({ title: 'Feil', description: 'Klarte ikke slette listen' });
+    } finally {
+      setDialogOpen(false);
+      setSelectedList(null);
+    }
+  };
+
   if (!isLoaded) {
-    return <LoadingSpinner />;
+    return (
+      <div className='flex justify-center items-center mx-auto'>
+        <LoadingSpinner />
+      </div>
+    );
   }
 
   if (!isSignedIn) {
     return (
-      <div>
-        <p>Logg inn for å se dine lister</p>
+      <div className='flex justify-center items-center mx-auto'>
+        <p className='font-serif'>Logg inn for å se dine lister</p>
       </div>
     );
   }
 
   return (
-    <div className='space-y-4'>
-      <h3 className='font-sans font-bold text-xl'>Dine lister</h3>
+    <div className='space-y-6'>
+      <Badge className='font-sans text-lg bg-secondary/40'>Dine lister</Badge>
 
-      {/* Create List Form */}
       <form onSubmit={createNewList} className='flex space-x-2'>
         <Input
           type='text'
           value={newListName}
           onChange={(e) => setNewListName(e.target.value)}
-          placeholder='Navn på ny liste'
+          placeholder='Ny liste'
           className='text-base font-serif'
           required
         />
-
         <Button variant='secondary' type='submit' disabled={creatingList}>
           {creatingList ? <LoadingSpinner /> : <Plus />}
         </Button>
       </form>
 
-      {/* List Display */}
       {loadingLists ? (
         <LoadingSpinner />
       ) : (
         <div className='flex flex-col space-y-4'>
           {lists.map((list) => (
-            <Link href={`/list/${list._id}`} key={list._id}>
-              <Card>
+            <Card
+              key={list._id}
+              className='flex justify-between items-center px-2'
+            >
+              <Link href={`/list/${list._id}`} className='flex-grow'>
                 <CardHeader>
-                  <CardTitle className='font-serif'>{list.name}</CardTitle>
+                  <CardTitle className='font-serif text-lg'>
+                    {list.name}
+                  </CardTitle>
                 </CardHeader>
-              </Card>
-            </Link>
+              </Link>
+              <Button
+                className='font-sans'
+                variant='destructive'
+                onClick={() => handleDeleteClick(list)}
+              >
+                Slett liste
+              </Button>
+            </Card>
           ))}
         </div>
       )}
+
+      <Dialog open={isDialogOpen} onOpenChange={setDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className='font-sans'>Bekreft sletting</DialogTitle>
+            <DialogDescription className='font-serif'>
+              Er du sikker på at du vil slette listen? Denne handlingen er
+              permanent.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter className='flex flex-col sm:flex-row sm:items-center space-y-2 sm:space-x-2 sm:space-y-0'>
+            <Button
+              className='font-serif'
+              variant='outline'
+              onClick={() => setDialogOpen(false)}
+            >
+              Avbryt
+            </Button>
+            <Button
+              className='font-serif'
+              variant='destructive'
+              onClick={confirmDelete}
+            >
+              Slett
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,51 @@
 import Dashboard from './components/Dashboard';
-export default function Home() {
+import { SignInButton } from '@clerk/nextjs';
+import { currentUser } from '@clerk/nextjs/server';
+import { Card, CardContent, CardTitle } from '@/components/ui/card';
+import { FileText, Users } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+
+export default async function Home() {
+  const user = await currentUser();
+
+  if (!user) {
+    return (
+      <div className='flex flex-col mx-auto justify-center items-center mt-8 p-8'>
+        <h2 className='font-mono text-3xl'>LISTA</h2>
+        <p className='font-sans font-semibold'>
+          Din eneste app for dine lister
+        </p>
+        <div className='flex flex-col space-y-4 mt-8 justify-items-center'>
+          <Card>
+            <CardTitle className='flex justify-items-center font-sans font-semibold text-lg p-6'>
+              <FileText className='mr-2' />
+              Flere lister på en plass
+            </CardTitle>
+            <CardContent className='font-serif'>
+              Lag flere lister for forskjellige gjøremål og ha de enkelt samlet
+              i en og samme app
+            </CardContent>
+          </Card>
+          <Card>
+            <CardTitle className='flex justify-items-center font-sans font-semibold text-lg p-6'>
+              <Users className='mr-2' />
+              Del listene dine
+            </CardTitle>
+            <CardContent className='font-serif'>
+              Inviter andre brukere til å bruke listene dine og ha full kontroll
+              på alle punkt
+            </CardContent>
+          </Card>
+          <SignInButton>
+            <Button className='w-full font-sans font-semibold'>
+              Kom i gang
+            </Button>
+          </SignInButton>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <main className='flex min-h-screen flex-col items-center p-4 max-w-3xl mx-auto mt-4'>
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm'>
