bba53fc66075ddb9404d34a6524d6e04fbd1698f
martinbolsnes <martinbolsones@gmail.com>
2025-02-15 20:35:00 +0100

    edit layout

--- app/list/[id]/page.tsx
+++ app/list/[id]/page.tsx
@@ -1,11 +1,11 @@
-import ListComponent from '../../components/ListComponent';
+import InteractiveListComponent from '../../components/InteractiveListComponent';
 
 export default function ListPage({ params }: { params: { id: string } }) {
   return (
     <main className='flex min-h-screen flex-col items-center p-4 max-w-3xl mx-auto mt-4'>
-      <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm'>
+      <div className='w-full items-center justify-between font-mono text-sm'>
         <h1 className='hidden'>LISTA</h1>
-        <ListComponent listId={params.id} />
+        <InteractiveListComponent listId={params.id} />
       </div>
     </main>
   );
