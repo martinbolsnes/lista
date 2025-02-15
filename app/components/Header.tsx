c69fe2d5763775767a7cc2964651d6bede6eb30c
martinbolsnes <martinbolsones@gmail.com>
2025-02-15 20:34:13 +0100

    edit layout

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -18,68 +18,56 @@ export default async function Header() {
       <Link href='/'>
         <h1 className='font-mono text-3xl text-background'>LISTA</h1>
       </Link>
-      <div className='flex items-center gap-2'>
-        <div>
-          <div className='flex space-x-2'>
-            <SignedOut>
-              <SignInButton>
-                <Button variant='outline' size='sm'>
-                  Logg inn
-                </Button>
-              </SignInButton>
-
-              {/* <SignUpButton>
-                <Button variant='secondary'size='sm'></Button>
-              </SignUpButton> */}
-            </SignedOut>
-          </div>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </div>
-        <div>
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button variant='ghost'>
-                <Menu className={cn('w-6 h-6')} />
-              </Button>
-            </SheetTrigger>
-            <SheetContent
-              className={cn('bg-background/95 border border-border')}
-            >
-              <SheetHeader className={cn('flex text-left')}>
-                <SheetTitle>
-                  <Link href='/'>
-                    <p
-                      className={cn(
-                        `text-md sm:text-lg font-bold font-mono -mb-2`
-                      )}
-                    >
-                      LISTA
-                    </p>
-                  </Link>
-                </SheetTitle>
-                <SheetDescription className='hidden'>LISTA</SheetDescription>
-              </SheetHeader>
-              <div className={cn('flex flex-col items-end gap-4 py-6 text-lg')}>
-                <SheetTrigger asChild>
-                  <Link href='/'>
-                    <Button variant='ghost' className='font-semibold'>
-                      Hjem
-                    </Button>
-                  </Link>
-                </SheetTrigger>
-                <SheetTrigger asChild>
-                  <Link href='/dashboard'>
-                    <Button variant='ghost' className='font-semibold'>
-                      Dine lister
-                    </Button>
-                  </Link>
-                </SheetTrigger>
-              </div>
-            </SheetContent>
-          </Sheet>
-        </div>
+      <div className='flex items-center space-x-2'>
+        <SignedOut>
+          <SignInButton>
+            <Button variant='outline' size='sm'>
+              Logg inn
+            </Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant='ghost'>
+              <Menu className={cn('w-6 h-6')} />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className={cn('bg-background/95 border border-border')}>
+            <SheetHeader className={cn('flex text-left')}>
+              <SheetTitle>
+                <Link href='/'>
+                  <p
+                    className={cn(
+                      `text-md sm:text-lg font-bold font-mono -mb-2`
+                    )}
+                  >
+                    LISTA
+                  </p>
+                </Link>
+              </SheetTitle>
+              <SheetDescription className='hidden'>LISTA</SheetDescription>
+            </SheetHeader>
+            <div className={cn('flex flex-col items-end gap-4 py-6 text-lg')}>
+              <SheetTrigger asChild>
+                <Link href='/'>
+                  <Button variant='ghost' className='font-semibold'>
+                    Hjem
+                  </Button>
+                </Link>
+              </SheetTrigger>
+              <SheetTrigger asChild>
+                <Link href='/lists'>
+                  <Button variant='ghost' className='font-semibold'>
+                    Dine lister
+                  </Button>
+                </Link>
+              </SheetTrigger>
+            </div>
+          </SheetContent>
+        </Sheet>
       </div>
     </div>
   );
