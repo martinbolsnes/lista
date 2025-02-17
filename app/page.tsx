6843056e6cb745c6d9eb6bfacf85bd504653e4cd
martinbolsnes <martinbolsones@gmail.com>
2025-02-17 16:47:27 +0100

    remove z-index

--- app/page.tsx
+++ app/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
 
   return (
     <main className='flex min-h-screen flex-col items-center p-4 max-w-3xl mx-auto mt-4'>
-      <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm'>
+      <div className='max-w-5xl w-full items-center justify-between font-mono text-sm'>
         <Dashboard />
       </div>
     </main>
